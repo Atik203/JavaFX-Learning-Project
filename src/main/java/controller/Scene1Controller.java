bf2791569d0b2f38f7c9515a41c54f9e33acb85f
Atikur Rahaman <atikurrahaman0305@gmail.com>
2025-03-01 19:39:27 +0600

    update

--- src/main/java/controller/Scene1Controller.java
+++ src/main/java/controller/Scene1Controller.java
@@ -27,7 +27,7 @@ public class Scene1Controller {
     }
 
     public void switchToScene1(ActionEvent e) throws IOException {
-        root = FXMLLoader.load(getClass().getResource("/view/scene1.fxml"));
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/view/scene1.fxml")));
         stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
